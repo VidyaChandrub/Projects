bdb3f5cac56d599bd06925c115c580cbdd0e9d88
Vidya <vidyac266@gmail.com>
2023-09-16 21:33:57 +0530

    okie

--- JAVA NEW PROJECT/JAVA_BASICS/Var.java
+++ JAVA NEW PROJECT/JAVA_BASICS/Var.java
@@ -1,37 +1,27 @@
 class Var{
-    int b = 80; //instance variables
-
-    static int c=30;
-
+    int a = 100; //instance variable
+    static int b = 100; //static variable
     public static void main(String args[]){
 
+        int c = 10;
         Var v1 = new Var();
         Var v2 = new Var();
 
-        int a = 10; //local variables
-        System.out.println(a);
-
-        
+        System.out.println(v1.a);
 
         System.out.println(v1.b);
-        System.out.println(v1.c);
-
-        System.out.println(a+v1.b+v1.c);
-        System.out.println(a+v2.b+v2.c);
-
-        System.out.println(c);
-        System.out.println(Var.c);
-        System.out.println(v2.b);
-        System.out.println(v2.c);
-
-        v2.b = 300;  // value of instance variables vary from value to value
-
-        System.out.println(a+v1.b+v1.c);
-        System.out.println(a+v2.b+v2.c); 
+        System.out.println(v1.a);
+    
+        System.out.println(b);
         
-        v1.c = 100;   // value of static variables will not change 
+        v1.a = 200;
+        System.out.println(v1.a);
+        System.out.println(v2.a);
 
-        System.out.println(a+v1.b+v1.c);
-        System.out.println(a+v2.b+v2.c);
+        v1.b = 30;
+        System.out.println(v1.b);
+        System.out.println(v2.b);
+
+
     }
 }
