ad80961da843f8a651e26160d87959686f92bc31
Vidya <vidyac266@gmail.com>
2023-11-01 22:05:28 +0530

    objects

---
+++ JAVA NEW PROJECT/Java/Oops/Test1.java
@@ -0,0 +1,11 @@
+public class Test1{
+    //static int id;
+    String name;
+
+    public static void main(String args[]){
+        int id;
+        Test1 t = new Test1();
+        System.out.println(t.id);
+        System.out.println(t.name);
+    }
+}
