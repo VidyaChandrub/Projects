51845e53ffda6af9c34ba140a7934778d4cd4510
Vidya <vidyac266@gmail.com>
2023-11-03 22:15:32 +0530

    api

---
+++ JAVA NEW PROJECT/APIcreation/HelloController.java
@@ -0,0 +1,5 @@
+public class HelloController{
+    // public static void main(String args[]){
+
+    // }
+}
--- JAVA NEW PROJECT/Java/Oops/Inheritance/MethodOverloading.java
+++ JAVA NEW PROJECT/Java/Oops/Inheritance/MethodOverloading.java
@@ -1,15 +1,19 @@
 class Parent{
-    public int m1(){
+    public void m1(){
         System.out.println("P m1 method");
     }
 }
-class Child extends Parent{
+class Child {
     public void m1(){
         System.out.println("C m1 method");
     }
 }
-public class MethodOverloading{
+public class MethodOverloading extends Child{
     public static void main(String args[]){
-
+        Child c = new Child();
+        Parent p = new Parent();
+        
+        p.m1();
+        c.m1();
     }
 }
