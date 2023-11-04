d9eb02b4849620dd64097ab8e5e20929a403cdde
Vidya <vidyac266@gmail.com>
2023-11-04 20:41:49 +0530

    function expression

---
+++ JAVASCRIPT/Practice/functions/anonyms.js
@@ -0,0 +1,5 @@
+let X = function (num){
+    return num*num;
+}
+
+console.log(X(6));
---
+++ JAVASCRIPT/Practice/functions/greet.js
@@ -0,0 +1,20 @@
+// function greet(){
+//     console.log("Hello....")
+// }
+
+// greet();
+
+// function greet2(name){
+//         console.log("Hello "+ name);
+// }
+
+// //var name1 = "vidya";
+// let name1 = prompt("Enter a name ..")
+// greet2(name1);
+
+function add(a, b){
+    return a+b;
+}
+
+let x = add(5, 6);
+console.log(x);
