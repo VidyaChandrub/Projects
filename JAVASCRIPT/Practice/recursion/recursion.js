ce27427ddc8aa375041f1762a4e1de78949fec40
Vidya <vidyac266@gmail.com>
2023-11-06 22:11:47 +0530

    arrays

--- JAVASCRIPT/Practice/recursion/recursion.js
+++ JAVASCRIPT/Practice/recursion/recursion.js
@@ -11,3 +11,18 @@ function recurse(number){
     
 }
 recurse(8);
+
+let person = {
+    name: "vidya",
+    age : 78, 
+    name : "b"
+};
+
+console.log(person)
+
+let arr = [1,1,2,"vidya", false];
+console.log(arr)
+
+arr.forEach(element => {
+    console.log(element);
+});
