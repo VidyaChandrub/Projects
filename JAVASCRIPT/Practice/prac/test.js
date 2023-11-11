136a7b7d0a6b619d087f7036123dc9478a7818cd
Vidya <vidyac266@gmail.com>
2023-11-11 21:35:21 +0530

    use strict

---
+++ JAVASCRIPT/Practice/prac/test.js
@@ -0,0 +1,16 @@
+// 'use strict';
+// var name = "vidya";
+// console.log(name);
+
+// myvar = 10;
+// "use strict"; // doesnt work must be declared at the begining of the script
+// console.log(myvar);
+
+function wish(){
+    
+    "use strict"; // works in function
+    mywish = "GN";
+    console.log(mywish);
+}
+
+wish();
