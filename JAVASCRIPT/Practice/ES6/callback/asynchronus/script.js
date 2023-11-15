97b87743ade1a3a28ebc3bc3a33a45fd94ca685d
Vidya <vidyac266@gmail.com>
2023-11-15 20:21:14 +0530

    setTimeout

---
+++ JAVASCRIPT/Practice/ES6/callback/asynchronus/script.js
@@ -0,0 +1,18 @@
+// setTimeout(() => {
+//     console.log("Hello")
+// },3000)
+
+function greet(){
+    console.log("Good Morning")
+}
+setTimeout(greet,3000);
+
+function showTime(){
+    let datetime = new Date();
+    let time = datetime.toLocaleTimeString();
+    console.log(time);
+
+    setTimeout(showTime, 2000);
+}
+
+showTime();
