1cb7241e99a9c0ed26e1daee049b8d08040986a8
Vidya <vidyac266@gmail.com>
2023-11-16 22:10:28 +0530

    dom

--- JAVASCRIPT/Practice/DOM/script.js
+++ JAVASCRIPT/Practice/DOM/script.js
@@ -1,6 +1,12 @@
 const head1 = document.getElementById("heading1");
 const image1 = document.getElementById("img1");
+const dt = document.getElementById("date");
 
-head1.innerHTML = "Hello, Good Morning World !!";
+dt.innerHTML = "Date : " + Date();
 
-image1.src = "hellopic.png";
+document.write("Helloooooooooooo");
+
+//head1.innerHTML = "Hello, Good Morning World !!";
+
+// image1.src = "hellopic.png";
+
