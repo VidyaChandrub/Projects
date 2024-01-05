a7f0deff27971f8c544b325f4ff9c1b593e3aeaf
Vidya <vidyac266@gmail.com>
2024-01-05 20:23:19 +0530

    method overloading

--- JAVA NEW PROJECT/Practice/TestEx.java
+++ JAVA NEW PROJECT/Practice/TestEx.java
@@ -2,6 +2,11 @@ class A{
     public static void myMethod(){
         System.out.println("My method 1");
     }
+    
+    //method overload
+    public static void myMethod(int num){
+        System.out.println("Good Morning "+ num);
+    }
 
     public static void myMethod2(String name){
         System.out.println("Hello "+name);
@@ -14,6 +19,7 @@ public class TestEx {
     System.out.println("GE");
     A a = new A();
     a.myMethod();
+    a.myMethod(6);
     a.myMethod2("Vidya");
     }
 }
